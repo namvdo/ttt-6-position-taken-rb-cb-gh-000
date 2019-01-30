08964902ec2c0d7d105613e8d2277f1311d95ada
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 06:50:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,23 @@
 # code your #position_taken? method here!
 require_relative './spec/position_taken_spec.rb'
-board = [" "," "," "]
-def position_taken(board, index)
-  if board ==" "
-    return true
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-  end
+# code your input_to_index and move method here!
+input = "1", "2", "3", "4", "5", "6", "7", "8", "9"
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+board = [" "," "," "]
+def move(input, index, character = "X")
+  return input[index] = character
+end
+def position_taken(board, input[index])
 end
